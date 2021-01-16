8af23ef9d10e54b23f779c3de7e0b6120d75f51a
tailaiwang <tailaiwang01@gmail.com>
2021-01-16 14:29:24 -0500

    sample route

--- server/app/routes.py
+++ server/app/routes.py
@@ -17,6 +17,22 @@ def index():
     data = json.load(f)
     return jsonify(data)
 
+@app.route("/tickers/") # return unique tickers
+def titles():
+  script_dir = os.path.dirname(__file__)
+  full_path = os.path.join(script_dir, 'jobs/webScraper/scrapeResults.json')
+  with open(full_path) as f:
+    data = json.load(f)
+    tickerSet = set()
+    for line in data:
+      tickerSet.add(line['stockTicker'])
+
+    retval = []
+    for item in tickerSet:
+      retval.append(item)
+
+    return jsonify(retval)
+
 def runManualWebScrape():
   print("---------------------------")
   print("Running manual webscrape")

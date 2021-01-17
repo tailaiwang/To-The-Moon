ef9069efd4c57ff0bb93189ff7b672e1b0ec20f7
tailaiwang <tailaiwang01@gmail.com>
2021-01-17 01:06:30 -0500

    integrated FE and BE

--- app/src/components/BullList.js
+++ app/src/components/BullList.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 // import logo from ''
 
 // css
 import './BullList.css';
 
 const BullList = () => {
+
+    const [ data, setData ] = useState(null);
+
+    const getDataReq = {
+        method: "GET",
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        }
+    };
+
+    useEffect(() => {
+        fetch('/api/tickers', getDataReq)
+            .then(results => results.json())
+            .then(data => {
+                setData(data);
+            });
+    }, []);
+
     return(
-        <div className="bull-list-container">
-            List of Top Bullish Stocks based on Market Sentiment Analysis
-        </div>
+        <>
+            <div className="bull-list-container">
+                <p>
+                    List of Top Bullish Stocks based on Market Sentiment Analysis
+                </p>
+                <p>
+                    Data: {!data ? 'Loading...' : `${data}`}
+                </p>
+            </div>
+        </>
     );
 }
 
--- server/app/routes.py
+++ server/app/routes.py
@@ -13,36 +13,36 @@ from flask import jsonify
 from .jobs.webScraper import webScrapeToJSONAndPush
 
 
-# @app.route("/")
-# def index():
-#   script_dir = os.path.dirname(__file__)
-#   full_path = os.path.join(script_dir, 'jobs/webScraper/scrapeResults.json')
-#   with open(full_path) as f:
-#     data = json.load(f)
-#     return jsonify(data)
+@app.route("/api")
+def index():
+  script_dir = os.path.dirname(__file__)
+  full_path = os.path.join(script_dir, 'jobs/webScraper/scrapeResults.json')
+  with open(full_path) as f:
+    data = json.load(f)
+    return jsonify(data)
 
 
-# @app.route("/tickers/") # return unique tickers
-# def titles():
-#   script_dir = os.path.dirname(__file__)
-#   full_path = os.path.join(script_dir, 'jobs/webScraper/scrapeResults.json')
-#   with open(full_path) as f:
-#     data = json.load(f)
-#     tickerSet = set()
-#     for line in data:
-#       tickerSet.add(line['stockTicker'])
+@app.route("/api/tickers", methods=["GET"]) # return unique tickers
+def titles():
+  script_dir = os.path.dirname(__file__)
+  full_path = os.path.join(script_dir, 'jobs/webScraper/scrapeResults.json')
+  with open(full_path) as f:
+    data = json.load(f)
+    tickerSet = set()
+    for line in data:
+      tickerSet.add(line['stockTicker'])
 
-#     retval = []
-#     for item in tickerSet:
-#       retval.append(item)
+    retval = []
+    for item in tickerSet:
+      retval.append(item)
 
-#     return jsonify(retval)
+    return jsonify(retval)
 
-# @app.route("/live", methods=["GET"]) # return live DB
-# def live():
-#   headers = { 'Authorization': os.environ.get('DATABASE_ACCESS_KEY')}
-#   r = requests.get(os.environ.get('DATABASE_REST_API'), headers=headers)
-#   return r.text
+@app.route("/api/live", methods=["GET"]) # return live DB
+def live():
+  headers = { 'Authorization': os.environ.get('DATABASE_ACCESS_KEY')}
+  r = requests.get(os.environ.get('DATABASE_REST_API'), headers=headers)
+  return r.text
 
 def runAutomaticWebScrape():
     print("---------------------------")
@@ -62,14 +62,14 @@ def runManualWebScrape():
     webScrapeToJSONAndPush()
 
 
-@app.route('/scrape')
+@app.route('/api/scrape')
 def manualScrape():
     schedular.add_job(func=runManualWebScrape,
                       trigger='date', id='webScrapeManual')
     return "Added manual scraping to job...results will be updated in approx 30 minutes"
 
 
-@app.route("/ratings")
+@app.route("/api/ratings")
 def getAllRatings():
     ratings = []
 
@@ -87,7 +87,7 @@ def getAllRatings():
     return ratings.__str__()
 
 
-@app.route("/ratings/<stockTicker>")
+@app.route("/api/ratings/<stockTicker>")
 def getRatingDetail(stockTicker):
     headers = {'Authorization': os.environ.get('DATABASE_ACCESS_KEY')}
     r = requests.get(os.environ.get('DATABASE_REST_API'), headers=headers)
